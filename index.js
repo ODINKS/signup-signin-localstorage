b2cbb3995cc1e46fdd064b8fc42db4081b21a6fd
Nicholas Odinaka <nicholas.okeke87@gmail.com>
2023-09-12 21:52:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+//SIGN UP
+//grab the form data
+
+function signup(event){
+    event.preventDefault()
+    const firstName = document.getElementById('first-name');
+    const lastName = document.getElementById('last-name');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirm-password');
+    //Check if the passwords match
+    if(password.value !== confirmPassword.value){
+        alert(`passwords do not match`)
+        return
+    }
+    //Collecting data from the form
+    const user = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        password: password.value
+    };
+    localStorage.setItem('userData', JSON.stringify(user));
+    firstName.value = '';
+    lastName.value = '';
+    email.value = '';
+    password.value = '';
+    confirmPassword.value = '';
+    alert(`You successfully signed up!`);
+    //Redirect to the signin page
+    window.location.href = '/signin.html'
+
+}
+
+//Sign Up form
+function signin(event){
+    event.preventDefault();
+    const signEmail = document.getElementById('sign-emal') 
+    const signPassword = document.getElementById('sign-password') 
+    
+    //Retrieve user credentials from local storage
+    let user = localStorage.getItem('userData')
+    user = JSON.parse(user);
+    if(user.password !== signPassword.value){
+        alert(`Incorrect username or password!`);
+        return;
+    }
+
+    alert(`Signin successful!!!`)
+    signEmail.value = '';
+    signPassword.value = '';
+}
